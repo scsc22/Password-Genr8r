cbc4355ed59163b52325e80083fbae2ea57a7f65
Sean.Connor <132519718+scsc22@users.noreply.github.com>
2023-07-07 19:41:57 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+// Generate a random password
+function generatePassword() {
+  var length = getRandomNumber(8, 128); // Length of the generated password between 8 and 128
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?"; // Characters to include in the password
+  var password = "";
+
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  return password;
+}
